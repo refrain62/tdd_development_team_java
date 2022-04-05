c97a520f9ef2ec1d8caeae6feaad0b77b4555ce9
nao <nao_h13@hotmail.com>
2022-04-05 17:57:18 +0900

    認証処理 テスト 実装

---
+++ src/main/java/auth_lib/AuthenticatorImpl.java
@@ -0,0 +1,17 @@
+package auth_lib;
+
+/**
+ * 認証処理
+ */
+public class AuthenticatorImpl implements Authenticator
+{
+    /**
+     * 認証処理
+     */
+    @Override
+    public boolean auth( String uid, String pwd )
+    {
+        return false;
+    }
+    
+}
---
+++ src/test/java/auth_lib/AuthenticatorImplTest.java
@@ -0,0 +1,40 @@
+package auth_lib;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 認証処理 テスト
+ */
+public class AuthenticatorImplTest
+{
+    /**
+     * 認証処理
+     */
+    private AuthenticatorImpl _auth;
+
+    /**
+     * 前処理
+     */
+    @Before
+    public void setUp()
+    {
+        this._auth = new AuthenticatorImpl();
+
+        // 登録
+        this._auth.register( "taro", "pass1" );
+    }
+
+    /**
+     * 認証成功テスト
+     */
+    @Test
+    public void auth_success()
+    {
+        boolean result = this._auth.auth( "taro", "pass1" );
+
+        assertTrue( result );
+    }
+}
