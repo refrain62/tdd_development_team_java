c90ef67bd361ebc1e76b07a1db08c8a2d873daf1
nao <nao_h13@hotmail.com>
2022-04-05 19:10:37 +0900

    認証処理モック テスト 実装

---
+++ src/test/java/auth_lib/MockAuthenticatorTest.java
@@ -0,0 +1,52 @@
+package auth_lib;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+
+import auth_lib.AuthParameters.ParamKey;
+
+/**
+ * 認証処理モック テスト
+ */
+public class MockAuthenticatorTest
+{
+    /**
+     * 認証処理モック
+     */
+    private MockAuthenticator _auth;
+
+    /**
+     * 事前処理
+     */
+    @Before
+    public void setUp()
+    {
+        this._auth = new MockAuthenticatorTest();
+
+        this._auth.register( "taro", "pass1" );
+    }
+
+    /**
+     * 旧認証処理成功テスト
+     */
+    public void auth_success_old()
+    {
+        boolean result = this._auth.auth( "taro", "pass1" );
+
+        assertTrue( result );
+    }
+
+    /**
+     * 認証成功 テスト
+     */
+    public void auth_success()
+    {
+        AuthParameters authp = new AuthParameters();
+
+        authp.addParameter( ParamKey.Uid, "taro" );
+        authp.addParameter( ParamKey.Pwd, "pass1" );
+
+        assertTrue( this._auth.auth( athup ) );
+    }
+}
